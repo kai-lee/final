84ecaa8755ca8bfa86e43c06bb615ec88d46b9a4
kai-lee <bbruesch@mail.csuchico.edu>
2013-12-02 19:42:44 -0800

    functional sort

--- gamerev/app/controllers/home_controller.rb
+++ gamerev/app/controllers/home_controller.rb
@@ -1,5 +1,21 @@
 class HomeController < ApplicationController
+helper_method :sort_column, :sort_direction
   def index
-	@games = Game.all
-  end
+#	@games = Game.all
+	@games = Game.order(sort_column + " " + sort_direction) 
+
+ end
+
+#private 
+
+def sort_column
+	Game.column_names.include?(params[:sort]) ? params[:sort] : "name"
+end
+
+def sort_direction
+	%w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+end
+
+
+
 end
